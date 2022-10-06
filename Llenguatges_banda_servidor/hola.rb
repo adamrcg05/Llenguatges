8889ec767b31aef027949f2e32e03020b92df164
adamrcg05 <acalvo20206@iesjoanramis.org>
2022-10-06 08:51:05 +0200

    primer repositori

---
+++ Llenguatges_banda_servidor/hola.rb
@@ -0,0 +1,13 @@
+puts "Hola, Ramis!"
+
+class HolaRamis
+    def initialize()
+    end
+    def saluda()
+        puts "Hola, Ramis!"
+    end
+end
+
+objeto = HolaRamis.new()
+objeto.saluda
+gets()
